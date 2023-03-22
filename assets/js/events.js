9d3d963153962bb7b431af1decd6925b8c20d5a0
AKWEULCUE <mileidy1998@hotmail.com>
2023-03-22 11:47:23 -0500

    se finaliza funcion la cual crea las tarjetas

--- assets/js/events.js
+++ assets/js/events.js
@@ -5,41 +5,43 @@ const content = document.getElementById("card");
 let pageIndex = 0;
 let itemsPerPage = 4;
 
+loadItems();
 
 function loadItems(){
  
     content.innerHTML = "";
     for (let i = pageIndex * itemsPerPage; i <(pageIndex * itemsPerPage) + itemsPerPage; i++) {
-       const nam = data.eventos[i].name;
+       const name = data.eventos[i].name;
        const img = data.eventos[i].image;
        const desc = data.eventos[i].description;
        const price = data.eventos[i].price;
 
-       if( !data.eventos [i]) {
-        const item = document.createElement ("div");
+       if(!data.eventos[i]) {break}
+        const item = document.createElement("div");
         item.className = "rangoC";
-        item.innerHTML+= `
+        item.innerHTML+=`
         
         <div class="col-lg-3 col-sm-6">
                 <div class="card p-3">
-                    <img src="assets/images/Cine7.jpg" class="card-img-top shadow-lg bg-body-tertiary rounded"
-                        alt="...">
+                    <img src="${img}" class="card-img-top shadow-lg bg-body-tertiary rounded"
+                        alt="${name}">
                     <div class="card-body">
-                        <h5 class="card-title text-center">Title</h5>
-                        <p class="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h5 class="card-title text-center">${name}</h5>
+                        <p class="card-text text-center">${desc}</p>
                         <br>
                         <div class="d-flex justify-content-between">
-                            <p class="card-text"><small class="text-muted">Price $200</small></p>
+                            <p class="card-text"><small class="text-muted">Price $ ${price}</small></p>
                             <a href="pages/details.html" class="btn btn-primary float-right">View details</a>
                         </div>
                     </div>
                 </div>
             </div> `;
 
+
+            content.appendChild(item);
        }
        
     }
-}
 
 
 
@@ -65,23 +67,4 @@ function loadItems(){
 
 
 
-
-
-
-
-<div class="col-lg-3 col-sm-6">
-                <div class="card p-3">
-                    <img src="assets/images/Cine7.jpg" class="card-img-top shadow-lg bg-body-tertiary rounded"
-                        alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Title</h5>
-                        <p class="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <br>
-                        <div class="d-flex justify-content-between">
-                            <p class="card-text"><small class="text-muted">Price $200</small></p>
-                            <a href="pages/details.html" class="btn btn-primary float-right">View details</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
